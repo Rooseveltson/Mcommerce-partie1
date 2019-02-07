3434e16d3f71e216f5c36cf18bbceee1b14282e2
Cebeat Rooseveltson <c.rooseveltson@oodrive.com>
2019-02-07 13:51:11 +0100

    Validation du prix de vente

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -18,9 +18,6 @@ public interface ProductDao extends JpaRepository<Product, Integer> {
     List<Product> calculerMargeProduit();
 
     // Partie 2 tri par ordre alphabétique
-/*    @Query("SELECT id, nom, prix, prixDachat FROM Product ORDER BY nom ASC")
-    List<Product> findAll();*/
-
     @Query("SELECT id, nom, prix FROM Product ORDER BY nom ASC")
     List<Product> findAllByFirstnameOrderByFirstnameAsc();
 
